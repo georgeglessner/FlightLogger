59363a2653a3300af2c47717edf85849b7b75fb2
georgeglessner <g.glessner@outlook.com>
2020-02-15 01:54:02 -0700

    Initial

---
+++ FlightLogger.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from datetime import date
+from config import IP_ADDR
+import requests
+import os
+import csv
+
+filename = "flights.csv"
+
+# check if files exists
+file_exists = os.path.isfile(filename)
+
+# get all flights tracked to check against for duplicates
+flights = ""
+if file_exists:
+    with open(filename, "r") as fp:
+        flights = fp.read()
+
+today = date.today()
+time = datetime.now().strftime("%H:%M:%S")
+
+# get json data
+json_dump = requests.get(f"http://{IP_ADDR}:8080/data/aircraft.json").json()
+
+# write unique flights for the day to csv file
+with open(filename, "a", newline="") as file:
+    writer = csv.writer(file)
+    fieldnames = ["flight", "date", "time"]
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    # create headers if first time writing to file
+    if not file_exists:
+        writer.writeheader()
+
+    # add unique flights for the day
+    for entry in json_dump["aircraft"]:
+        # print( f'{entry['flight'].strip()}, {date.today()}')
+        if "flight" in entry and f'{entry["flight"].strip()},{today}' not in flights:
+            writer.writerow(
+                {"flight": entry["flight"].strip(), "date": today, "time": time}
+            )
---
+++ README.md
@@ -0,0 +1,15 @@
+# FlightLogger
+
+Store unique flights per day from dump1090 as a CSV file. 
+
+## Usage
+
+Before you can successfully run `FlightLogger.py` you will need to create a file called `config.py`. The file should be formatted as such:
+
+```
+IP_ADDR = 'Host IP Address'
+```
+
+The best use of this script would be to run it as a cron job or something similar. 
+
+Have a suggestion? [Please submit an issue](https://github.com/georgeglessner/FlightLogger/issues/new)! 
